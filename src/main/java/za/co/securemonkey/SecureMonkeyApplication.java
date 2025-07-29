0a755b13b3e09c8f0b6ab7f365452ec4264b4786
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2025-07-29 11:17:58 +0200

    chore: add boilerplate code for backend.

---
+++ src/main/java/za/co/securemonkey/SecureMonkeyApplication.java
@@ -0,0 +1,13 @@
+package za.co.securemonkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureMonkeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureMonkeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/za/co/securemonkey/SecureMonkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.securemonkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureMonkeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
