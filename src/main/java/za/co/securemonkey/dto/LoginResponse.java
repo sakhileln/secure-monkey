2f3fcb017fef35debfc1c38762b960674e18d70b
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-07-29 15:19:58 +0200

    feat(dto): add LoginResponse DTO

---
+++ src/main/java/za/co/securemonkey/dto/LoginResponse.java
@@ -0,0 +1,57 @@
+package za.co.securemonkey.dto;
+
+public class LoginResponse {
+    
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String email;
+    private String name;
+    
+    public LoginResponse(String token, Long id, String email, String name) {
+        this.token = token;
+        this.id = id;
+        this.email = email;
+        this.name = name;
+    }
+    
+    public String getToken() {
+        return token;
+    }
+    
+    public void setToken(String token) {
+        this.token = token;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+}
