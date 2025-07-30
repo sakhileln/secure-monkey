c6f60b8050e28f2e3732fc5c3669475c660cb5f7
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-07-31 00:50:00 +0200

    feat: add auth controller tests.

---
+++ src/test/java/za/co/securemonkey/controller/AuthControllerTest.java
@@ -0,0 +1,13 @@
+package za.co.securemonkey.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthControllerTest {
+
+    @Test
+    void contextLoads() {
+        // Simple test to verify Spring context loads
+    }
+}
